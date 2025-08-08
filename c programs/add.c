5cbe87b09c0368063b00c3f0e44c3991b051a4a9
Neeraj <165120154+ghostcoder911@users.noreply.github.com>
2025-08-08 20:00:59 +0530

    Update add.c
    demo changes done(test commit)

--- c programs/add.c
+++ c programs/add.c
@@ -1,5 +1,11 @@
 //Can you add all the functions to this file? the function definition and function call of all the 4 functions must be in a single file.
 #include <stdio.h>
+void sub(int c, int d){
+    int diff=0;
+    dif=c-d;
+    printf("dif of 2 numbers=%d",dif);
+
+}
 void add(int c, int d){
     int sum=0;
     sum=c+d;
@@ -8,5 +14,6 @@ void add(int c, int d){
 }
     int main(){
         add(20,60);
+        sub(20,10);
         return 0;
     }
