2474abe604d47c37f1951e03b5982fd1c2989c50
Neeraj <165120154+ghostcoder911@users.noreply.github.com>
2025-08-08 18:32:14 +0530

    Update add.c
    Code review point

--- c programs/add.c
+++ c programs/add.c
@@ -1,3 +1,4 @@
+//Can you add all the functions to this file? the function definition and function call of all the 4 functions must be in a single file.
 #include <stdio.h>
 void add(int c, int d){
     int sum=0;
