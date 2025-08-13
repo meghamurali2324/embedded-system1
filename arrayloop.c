cd2dc21655f246181babdd0eb5fedd304d7ab979
MEGHA V K <meghamurali2324@gmail.com>
2025-08-13 15:05:45 +0530

    Add files via upload

---
+++ arrayloop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+   
+    int num[]={5,10,15,20};
+    int i;
+
+    
+    for ( i = 0; i <4; i++)
+    {
+        printf("%d\n",num[i]);
+    }
+        return 0;
+    
+    
+}
